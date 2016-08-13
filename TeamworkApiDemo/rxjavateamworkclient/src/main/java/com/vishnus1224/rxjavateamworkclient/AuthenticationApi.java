cc6b80ecbc1fa4d54fbe2295bb57db1451e84e29
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-08-13 15:59:31 +0530

    authentication api to authenticate an user

---
+++ TeamworkApiDemo/rxjavateamworkclient/src/main/java/com/vishnus1224/rxjavateamworkclient/AuthenticationApi.java
@@ -0,0 +1,15 @@
+package com.vishnus1224.rxjavateamworkclient;
+
+import com.vishnus1224.rxjavateamworkclient.model.AccountResponse;
+
+import retrofit2.http.GET;
+import rx.Observable;
+
+/**
+ * Created by Vishnu on 8/13/2016.
+ */
+public interface AuthenticationApi {
+
+    @GET("/authenticate.json")
+    Observable<AccountResponse> authenticate();
+}
