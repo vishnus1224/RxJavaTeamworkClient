a5f8f7800cdf6adaeaff5eb7a857515e30f2d138
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-08-13 13:18:20 +0530

    created layout for the login activity and setup the views

--- TeamworkApiDemo/app/src/main/java/com/vishnus1224/teamworkapidemo/ui/activity/LoginActivity.java
+++ TeamworkApiDemo/app/src/main/java/com/vishnus1224/teamworkapidemo/ui/activity/LoginActivity.java
@@ -2,17 +2,53 @@ package com.vishnus1224.teamworkapidemo.ui.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.vishnus1224.teamworkapidemo.R;
 
 /**
  * Created by Vishnu on 8/13/2016.
  */
-public class LoginActivity extends BaseActivity {
+public class LoginActivity extends BaseActivity implements View.OnClickListener {
+
+    private EditText apiKeyEditText;
+
+    private Button loginButton;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        setupViews();
+    }
+
+    private void setupViews() {
+
+        apiKeyEditText = (EditText) findViewById(R.id.apiTokenEditText);
+
+        loginButton = (Button) findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()){
+
+            case R.id.loginButton:
+
+                loginButtonClicked();
+
+                break;
+
+        }
+    }
+
+    private void loginButtonClicked() {
+
     }
 }
