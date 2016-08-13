686cd4c9928958b0aa1115f8b6cc48c23a96d002
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-08-13 19:27:55 +0530

    application class

---
+++ TeamworkApiDemo/app/src/main/java/com/vishnus1224/teamworkapidemo/TeamworkApiDemo.java
@@ -0,0 +1,14 @@
+package com.vishnus1224.teamworkapidemo;
+
+import android.app.Application;
+
+/**
+ * Created by Vishnu on 8/13/2016.
+ */
+public class TeamworkApiDemo extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
