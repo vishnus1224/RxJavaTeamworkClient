fca57af8dd0b541b48ef223ebc455c23c932a321
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-08-13 16:03:29 +0530

    account model

---
+++ TeamworkApiDemo/rxjavateamworkclient/src/main/java/com/vishnus1224/rxjavateamworkclient/model/Account.java
@@ -0,0 +1,55 @@
+package com.vishnus1224.rxjavateamworkclient.model;
+
+import com.bluelinelabs.logansquare.annotation.JsonField;
+import com.bluelinelabs.logansquare.annotation.JsonObject;
+
+/**
+ * Created by Vishnu on 8/13/2016.
+ */
+@JsonObject
+public class Account {
+
+    @JsonField
+    private String name;
+
+    @JsonField
+    private String companyName;
+
+    @JsonField
+    private String userId;
+
+    @JsonField
+    private String logo;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getLogo() {
+        return logo;
+    }
+
+    public void setLogo(String logo) {
+        this.logo = logo;
+    }
+}
