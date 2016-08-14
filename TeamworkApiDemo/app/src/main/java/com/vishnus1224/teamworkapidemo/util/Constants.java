835e9160a88241874a68e5072db17082bb7eaea0
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-08-14 15:23:14 +0530

    constants class

---
+++ TeamworkApiDemo/app/src/main/java/com/vishnus1224/teamworkapidemo/util/Constants.java
@@ -0,0 +1,9 @@
+package com.vishnus1224.teamworkapidemo.util;
+
+/**
+ * Created by Vishnu on 8/14/2016.
+ */
+public class Constants {
+
+    public static final String INTENT_KEY_ACCOUNT = "account";
+}
