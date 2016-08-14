7a713487cba94fac11ce448f660c42622de317c6
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-08-14 11:50:38 +0530

    base view and login view

---
+++ TeamworkApiDemo/app/src/main/java/com/vishnus1224/teamworkapidemo/ui/view/BaseView.java
@@ -0,0 +1,7 @@
+package com.vishnus1224.teamworkapidemo.ui.view;
+
+/**
+ * Created by Vishnu on 8/14/2016.
+ */
+public interface BaseView {
+}
---
+++ TeamworkApiDemo/app/src/main/java/com/vishnus1224/teamworkapidemo/ui/view/LoginView.java
@@ -0,0 +1,12 @@
+package com.vishnus1224.teamworkapidemo.ui.view;
+
+/**
+ * Created by Vishnu on 8/14/2016.
+ */
+public interface LoginView extends BaseView {
+
+    void showProgressDialog();
+
+    void hideProgressDialog();
+
+}
