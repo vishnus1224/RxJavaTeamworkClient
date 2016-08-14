663754c1eccbddae57e97ab2e68a1db4f5914ff2
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-08-14 10:45:11 +0530

    injected the application component

--- TeamworkApiDemo/app/src/main/java/com/vishnus1224/teamworkapidemo/TeamworkApiDemo.java
+++ TeamworkApiDemo/app/src/main/java/com/vishnus1224/teamworkapidemo/TeamworkApiDemo.java
@@ -2,13 +2,30 @@ package com.vishnus1224.teamworkapidemo;
 
 import android.app.Application;
 
+import com.vishnus1224.teamworkapidemo.di.component.ApplicationComponent;
+import com.vishnus1224.teamworkapidemo.di.component.DaggerApplicationComponent;
+import com.vishnus1224.teamworkapidemo.di.module.ApplicationModule;
+
 /**
  * Created by Vishnu on 8/13/2016.
  */
 public class TeamworkApiDemo extends Application {
 
+    private ApplicationComponent applicationComponent;
+
     @Override
     public void onCreate() {
         super.onCreate();
+
+        injectApplicationComponent();
+    }
+
+    private void injectApplicationComponent() {
+
+        applicationComponent = DaggerApplicationComponent.builder()
+                .applicationModule(new ApplicationModule(this))
+                .build();
+
+        applicationComponent.inject(this);
     }
 }
