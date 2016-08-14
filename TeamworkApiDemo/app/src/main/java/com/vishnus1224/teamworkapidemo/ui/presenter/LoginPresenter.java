565d23a5b1997e6fd6cff6572a10fc415335d0b8
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-08-14 16:27:25 +0530

    saving token and url to prefs once authenticated

--- TeamworkApiDemo/app/src/main/java/com/vishnus1224/teamworkapidemo/ui/presenter/LoginPresenter.java
+++ TeamworkApiDemo/app/src/main/java/com/vishnus1224/teamworkapidemo/ui/presenter/LoginPresenter.java
@@ -1,9 +1,13 @@
 package com.vishnus1224.teamworkapidemo.ui.presenter;
 
+import com.vishnus1224.rxjavateamworkclient.model.Account;
 import com.vishnus1224.rxjavateamworkclient.model.AccountResponse;
 import com.vishnus1224.teamworkapidemo.di.scope.PerActivity;
+import com.vishnus1224.teamworkapidemo.manager.PreferencesManager;
+import com.vishnus1224.teamworkapidemo.manager.TokenManager;
 import com.vishnus1224.teamworkapidemo.ui.view.LoginView;
 import com.vishnus1224.teamworkapidemo.usecase.UseCase;
+import com.vishnus1224.teamworkapidemo.util.Constants;
 
 import java.net.UnknownHostException;
 
@@ -22,11 +26,19 @@ public class LoginPresenter implements BasePresenter<LoginView> {
 
     private LoginView loginView;
 
+    private PreferencesManager preferencesManager;
+
+    private TokenManager tokenManager;
+
     @Inject
-    public LoginPresenter(@Named("authentication") UseCase useCase) {
+    public LoginPresenter(@Named("authentication") UseCase useCase, PreferencesManager preferencesManager,
+                          TokenManager tokenManager) {
 
         this.useCase = useCase;
 
+        this.preferencesManager = preferencesManager;
+
+        this.tokenManager = tokenManager;
     }
 
     @Override
@@ -45,16 +57,21 @@ public class LoginPresenter implements BasePresenter<LoginView> {
 
     }
 
-    public void loginUser() {
+    /**
+     * Attempt to authenticate the user.
+     * @param apiToken api token entered by the user.
+     */
+    public void loginUser(String apiToken) {
 
         loginView.showProgressDialog();
+
+        setToken(apiToken);
 
         useCase.execute(new Subscriber<AccountResponse>() {
 
             @Override
             public void onCompleted() {
 
-                hideProgressDialog();
 
             }
 
@@ -78,6 +95,13 @@ public class LoginPresenter implements BasePresenter<LoginView> {
             @Override
             public void onNext(AccountResponse accountResponse) {
 
+                saveTokenToPrefs();
+
+                saveUrlToPrefs(accountResponse.getAccount());
+
+                hideProgressDialog();
+
+                loginView.goToMainScreen(accountResponse.getAccount());
 
 
             }
@@ -85,9 +109,31 @@ public class LoginPresenter implements BasePresenter<LoginView> {
 
     }
 
+    /**
+     * Cancel the authentication flow.
+     */
     public void cancelLogin() {
 
         useCase.unSubscribe();
+
+    }
+
+
+    private void setToken(String apiToken) {
+
+        tokenManager.setApiToken(apiToken);
+
+    }
+
+    private void saveUrlToPrefs(Account account) {
+
+        preferencesManager.save(Constants.PREFS_KEY_SITE_URL, account.getURL());
+
+    }
+
+    private void saveTokenToPrefs() {
+
+        preferencesManager.save(Constants.PREFS_KEY_API_TOKEN, tokenManager.getApiToken());
 
     }
 
