915e9d54358fea4b8d28336b1773087e79962ea0
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-08-14 20:08:08 +0530

    setup the views and the list adapter

--- TeamworkApiDemo/app/src/main/java/com/vishnus1224/teamworkapidemo/ui/activity/MainActivity.java
+++ TeamworkApiDemo/app/src/main/java/com/vishnus1224/teamworkapidemo/ui/activity/MainActivity.java
@@ -1,15 +1,44 @@
 package com.vishnus1224.teamworkapidemo.ui.activity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
+import android.widget.ListView;
 
 import com.vishnus1224.teamworkapidemo.R;
 
 public class MainActivity extends BaseActivity {
+
+    private FrameLayout contentFrameLayout;
+
+    private ListView drawerItemsListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setupViews();
+
+        setDrawerListAdapter();
+
     }
+
+    private void setupViews() {
+
+        contentFrameLayout = (FrameLayout) findViewById(R.id.drawerContentFrame);
+
+        drawerItemsListView = (ListView) findViewById(R.id.drawerItemListView);
+
+    }
+
+    private void setDrawerListAdapter() {
+
+        String[] items = getResources().getStringArray(R.array.drawer_items);
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_expandable_list_item_1, items);
+
+        drawerItemsListView.setAdapter(arrayAdapter);
+    }
+
 }
