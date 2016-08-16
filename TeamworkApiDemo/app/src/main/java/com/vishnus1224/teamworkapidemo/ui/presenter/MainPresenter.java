cbaf099e8a27a2f7e52e37a5b72cab6d2ea5847e
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-08-16 20:41:46 +0530

    presenter for the main screen

---
+++ TeamworkApiDemo/app/src/main/java/com/vishnus1224/teamworkapidemo/ui/presenter/MainPresenter.java
@@ -0,0 +1,95 @@
+package com.vishnus1224.teamworkapidemo.ui.presenter;
+
+import com.vishnus1224.teamworkapidemo.ui.view.MainView;
+
+import javax.inject.Inject;
+
+/**
+ * Handles presentation logic for the main screen.
+ * Created by Vishnu on 8/16/2016.
+ */
+public class MainPresenter implements BasePresenter<MainView> {
+
+    private MainView mainView;
+
+    /**
+     * Item position of the currently selected navigation drawer item.
+     */
+    private int selectedItemPosition;
+
+    @Inject
+    public MainPresenter(){
+
+
+    }
+
+    @Override
+    public void onViewAttached(MainView mainView) {
+
+        if(mainView == null){
+
+            throw new IllegalArgumentException("View cannot be null");
+
+        }
+
+        this.mainView = mainView;
+
+    }
+
+    @Override
+    public void onViewDetached(MainView mainView) {
+
+        this.mainView = null;
+
+    }
+
+    public void drawerItemClicked(int position){
+
+        //do not take any action if the new position is same as the selectedItemPosition
+        if(selectedItemPosition == position){
+
+            return;
+
+        }
+
+        setSelectedItemPosition(position);
+
+        showScreen(position);
+
+
+    }
+
+    /**
+     * Show the correct screen based on the selected item position.
+     * @param position The position of the selected item.
+     */
+    private void showScreen(int position) {
+
+        switch (position){
+
+            case 0:
+
+                mainView.showLatestActivityScreen();
+
+                break;
+            case 1:
+
+                mainView.showProjectsScreen();
+
+                break;
+            case 2:
+
+                mainView.showTaskScreen();
+
+                break;
+
+        }
+
+    }
+
+    private void setSelectedItemPosition(int position) {
+
+        selectedItemPosition = position;
+
+    }
+}
