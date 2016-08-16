0a48e32bf2a2a0c739f3a5380fc5dfad496ed941
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-08-16 22:41:13 +0530

    model for representing an action bar title

---
+++ TeamworkApiDemo/app/src/main/java/com/vishnus1224/teamworkapidemo/model/ActionBarTitle.java
@@ -0,0 +1,19 @@
+package com.vishnus1224.teamworkapidemo.model;
+
+/**
+ * Created by Vishnu on 8/16/2016.
+ */
+public class ActionBarTitle {
+
+    private final String title;
+
+    public ActionBarTitle(String title){
+
+        this.title = title;
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
