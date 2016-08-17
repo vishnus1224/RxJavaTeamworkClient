e17e0c7e9ce0e7ca313df3e7caf5e56cb6c747a4
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-08-17 19:19:04 +0530

    method to hide soft keyboard

--- TeamworkApiDemo/app/src/main/java/com/vishnus1224/teamworkapidemo/ui/fragment/BaseFragment.java
+++ TeamworkApiDemo/app/src/main/java/com/vishnus1224/teamworkapidemo/ui/fragment/BaseFragment.java
@@ -1,9 +1,20 @@
 package com.vishnus1224.teamworkapidemo.ui.fragment;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 
 /**
  * Created by Vishnu on 8/14/2016.
  */
 public class BaseFragment extends Fragment {
+
+    protected void hideSoftKeyboard(View view){
+
+        InputMethodManager imm = (InputMethodManager)getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+
+    }
+
 }
