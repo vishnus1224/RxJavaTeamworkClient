41557b5175da69dbe06d3a2b32e379faf2315541
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-08-22 19:16:33 +0530

    interface for querying only starred entries

---
+++ TeamworkApiDemo/rxjavateamworkclient/src/main/java/com/vishnus1224/rxjavateamworkclient/client/Starred.java
@@ -0,0 +1,11 @@
+package com.vishnus1224.rxjavateamworkclient.client;
+
+/**
+ * Used for fetching only starred entries.
+ * Created by Vishnu on 8/22/2016.
+ */
+interface Starred {
+
+    void onlyStarred(boolean value);
+
+}
