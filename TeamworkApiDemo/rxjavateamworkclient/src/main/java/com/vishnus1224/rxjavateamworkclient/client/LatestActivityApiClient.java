9837a88acc03c65038e80c75a6928599e0bd6153
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-08-22 19:27:29 +0530

    api client for fetching the latest activity

---
+++ TeamworkApiDemo/rxjavateamworkclient/src/main/java/com/vishnus1224/rxjavateamworkclient/client/LatestActivityApiClient.java
@@ -0,0 +1,57 @@
+package com.vishnus1224.rxjavateamworkclient.client;
+
+import com.vishnus1224.rxjavateamworkclient.api.LatestActivityApi;
+import com.vishnus1224.rxjavateamworkclient.config.TeamworkApiConfig;
+import com.vishnus1224.rxjavateamworkclient.model.LatestActivityResponse;
+
+import java.util.List;
+
+import rx.Observable;
+
+/**
+ * Created by Vishnu on 8/22/2016.
+ */
+public class LatestActivityApiClient extends TeamworkApiClient implements Limitable, Starred {
+
+    private static final int DEFAULT_MAX_ITEMS = 60;
+
+    private static final int MAX_ITEM_LIMIT = 200;
+
+    private int maxItems = DEFAULT_MAX_ITEMS;
+
+    private boolean onlyStarred = false;
+
+    public LatestActivityApiClient(TeamworkApiConfig teamworkApiConfig) {
+        super(teamworkApiConfig);
+    }
+
+    public Observable<List<LatestActivityResponse>> getLatestActivity(){
+
+        return getRetrofit().create(LatestActivityApi.class).getLatestActivity(maxItems, onlyStarred);
+
+    }
+
+    @Override
+    public LatestActivityApiClient maxItems(int count) {
+
+        if(count < 0){
+
+            throw new IllegalArgumentException("MaxItems should be greater than 0");
+
+        }
+
+        maxItems = count > MAX_ITEM_LIMIT ? MAX_ITEM_LIMIT : count;
+
+        return this;
+
+    }
+
+    @Override
+    public LatestActivityApiClient onlyStarred(boolean value) {
+
+        onlyStarred = value;
+
+        return this;
+
+    }
+}
