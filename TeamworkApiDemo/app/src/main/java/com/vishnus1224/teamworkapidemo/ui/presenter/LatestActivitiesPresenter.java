76675cb5f337a974ba33d5a1812e1d2e1f2fcb51
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-08-24 16:51:54 +0530

    method to fetch latest activity from cloud

--- TeamworkApiDemo/app/src/main/java/com/vishnus1224/teamworkapidemo/ui/presenter/LatestActivitiesPresenter.java
+++ TeamworkApiDemo/app/src/main/java/com/vishnus1224/teamworkapidemo/ui/presenter/LatestActivitiesPresenter.java
@@ -1,10 +1,15 @@
 package com.vishnus1224.teamworkapidemo.ui.presenter;
 
+import com.vishnus1224.rxjavateamworkclient.model.LatestActivityResponse;
 import com.vishnus1224.teamworkapidemo.ui.view.LatestActivitiesView;
 import com.vishnus1224.teamworkapidemo.usecase.UseCase;
 
+import java.util.List;
+
 import javax.inject.Inject;
 import javax.inject.Named;
+
+import rx.Subscriber;
 
 /**
  * Created by Vishnu on 8/17/2016.
@@ -34,5 +39,29 @@ public class LatestActivitiesPresenter implements BasePresenter<LatestActivities
 
         this.latestActivitiesView = null;
 
+    }
+
+    public void fetchLatestActivityFromCloud(){
+
+        useCase.execute(new LatestActivityCloudSubscriber());
+
+    }
+
+    public static class LatestActivityCloudSubscriber extends Subscriber<List<LatestActivityResponse>>{
+
+        @Override
+        public void onCompleted() {
+
+        }
+
+        @Override
+        public void onError(Throwable e) {
+
+        }
+
+        @Override
+        public void onNext(List<LatestActivityResponse> latestActivityResponse) {
+
+        }
     }
 }
