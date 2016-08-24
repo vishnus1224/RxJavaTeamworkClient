e52d7fdbd04a404f8e29c434a2a8678ecfe76b58
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-08-24 15:06:28 +0530

    delegate for providing user component

---
+++ TeamworkApiDemo/app/src/main/java/com/vishnus1224/teamworkapidemo/delegate/UserComponentDelegate.java
@@ -0,0 +1,13 @@
+package com.vishnus1224.teamworkapidemo.delegate;
+
+import com.vishnus1224.teamworkapidemo.di.component.UserComponent;
+
+/**
+ * Provides the user component.
+ * Created by vishnu on 24/08/16.
+ */
+public interface UserComponentDelegate {
+
+    UserComponent provideUserComponent();
+
+}
