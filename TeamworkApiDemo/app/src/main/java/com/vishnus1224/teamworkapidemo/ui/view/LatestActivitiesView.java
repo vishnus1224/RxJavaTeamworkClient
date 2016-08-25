7c63fb885fc473dd672301e592be328685748c76
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-08-25 12:36:31 +0530

    methods for displaying ui elements

--- TeamworkApiDemo/app/src/main/java/com/vishnus1224/teamworkapidemo/ui/view/LatestActivitiesView.java
+++ TeamworkApiDemo/app/src/main/java/com/vishnus1224/teamworkapidemo/ui/view/LatestActivitiesView.java
@@ -1,7 +1,20 @@
 package com.vishnus1224.teamworkapidemo.ui.view;
 
+import com.vishnus1224.teamworkapidemo.model.Section;
+
+import java.util.List;
+
 /**
  * Created by Vishnu on 8/17/2016.
  */
 public interface LatestActivitiesView extends BaseView {
+
+    void showLatestActivity(List<Section> sections);
+
+    void showNoActivityView();
+
+    void showProgressBar();
+
+    void hideProgressBar();
+
 }
