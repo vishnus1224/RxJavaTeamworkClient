721ca18651226ab36b31385466eecd63a44afb4e
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-08-25 17:15:38 +0530

    adapter for displaying latest activity

---
+++ TeamworkApiDemo/app/src/main/java/com/vishnus1224/teamworkapidemo/ui/adapter/LatestActivitiesAdapter.java
@@ -0,0 +1,77 @@
+package com.vishnus1224.teamworkapidemo.ui.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.vishnus1224.rxjavateamworkclient.model.LatestActivityResponse;
+import com.vishnus1224.teamworkapidemo.R;
+import com.vishnus1224.teamworkapidemo.delegate.LatestActivityAdapterDelegate;
+import com.vishnus1224.teamworkapidemo.model.Section;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by vishnu on 25/08/16.
+ */
+public class LatestActivitiesAdapter extends RecyclerView.Adapter<LatestActivitiesAdapter.LatestActivityViewHolder> {
+
+    private List<Section<LatestActivityResponse>> sections = new ArrayList<>();
+
+    private LatestActivityAdapterDelegate latestActivityAdapterDelegate;
+
+    public LatestActivitiesAdapter() {
+
+        latestActivityAdapterDelegate = new LatestActivityAdapterDelegate(sections);
+
+    }
+
+    public void updateData(List<Section<LatestActivityResponse>> sections){
+
+        this.sections.clear();
+
+        this.sections.addAll(sections);
+
+        notifyDataSetChanged();
+
+    }
+
+    @Override
+    public LatestActivityViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        return latestActivityAdapterDelegate.onCreateViewHolder(parent, viewType);
+
+    }
+
+    @Override
+    public void onBindViewHolder(LatestActivityViewHolder holder, int position) {
+
+        latestActivityAdapterDelegate.onBindViewHolder(holder, position);
+
+    }
+
+    @Override
+    public int getItemCount() {
+
+        return sections.size();
+
+    }
+
+    public static class LatestActivityViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView titleTextView;
+        public LinearLayout itemContainer;
+
+        public LatestActivityViewHolder(View itemView) {
+            super(itemView);
+
+            titleTextView = (TextView) itemView.findViewById(R.id.adapterLatestActivitySectionTitle);
+
+            itemContainer = (LinearLayout) itemView.findViewById(R.id.adapterLatestActivityItemContainer);
+
+        }
+    }
+}
