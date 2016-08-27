36d6bc4c57d3293b2ce070bf8392fa987899affe
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-08-27 20:59:27 +0530

    handled exceptions that would arise when date or time are not in correct format

--- TeamworkApiDemo/rxjavateamworkclient/src/main/java/com/vishnus1224/rxjavateamworkclient/client/UpdatedAfterDate.java
+++ TeamworkApiDemo/rxjavateamworkclient/src/main/java/com/vishnus1224/rxjavateamworkclient/client/UpdatedAfterDate.java
@@ -1,5 +1,7 @@
 package com.vishnus1224.rxjavateamworkclient.client;
 
+import java.text.ParseException;
+
 /**
  * Allows for passing the updatedAfterDate param to a query.
  * Created by Vishnu on 8/27/2016.
@@ -10,6 +12,7 @@ interface UpdatedAfterDate<Type> {
      * If set then query will return results that are updated only after the specified date.
      * @param date date string in YYYYMMDDHHMMSS format.
      * @return object type that implements this interface.
+     * @throws ParseException if the date is in the wrong format.
      */
-    Type updatedAfterDate(String date);
+    Type updatedAfterDate(String date) throws ParseException;
 }
--- TeamworkApiDemo/rxjavateamworkclient/src/main/java/com/vishnus1224/rxjavateamworkclient/client/UpdatedAfterTime.java
+++ TeamworkApiDemo/rxjavateamworkclient/src/main/java/com/vishnus1224/rxjavateamworkclient/client/UpdatedAfterTime.java
@@ -1,5 +1,7 @@
 package com.vishnus1224.rxjavateamworkclient.client;
 
+import java.text.ParseException;
+
 /**
  * Allows for passing the updatedAfterTime param to a query.
  * Created by Vishnu on 8/27/2016.
@@ -10,6 +12,7 @@ interface UpdatedAfterTime<Type> {
      * Sets the updatedAfterTime query param.
      * @param timeString time string in HH:MM format.
      * @return type instance that implements the interface.
+     * @throws ParseException if the time is in the wrong format.
      */
-    Type updatedAfterTime(String timeString);
+    Type updatedAfterTime(String timeString) throws ParseException;
 }
