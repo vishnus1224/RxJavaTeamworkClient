caa33d3c7e7e759143cda0770083115dbee21a30
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-08-27 15:00:23 +0530

    listener for getting notified when any item in the dialog is clicked

---
+++ TeamworkApiDemo/app/src/main/java/com/vishnus1224/teamworkapidemo/listener/AddNewActivityItemClickListener.java
@@ -0,0 +1,21 @@
+package com.vishnus1224.teamworkapidemo.listener;
+
+/**
+ * Created by Vishnu on 8/27/2016.
+ */
+public interface AddNewActivityItemClickListener {
+
+    void addNewTaskClicked();
+
+    void addNewMessageClicked();
+
+    void addNewEventClicked();
+
+    void addNewMilestoneClicked();
+
+    void addNewTimeLogClicked();
+
+    void addNewNotebookClicked();
+
+    void addNewFileClicked();
+}
