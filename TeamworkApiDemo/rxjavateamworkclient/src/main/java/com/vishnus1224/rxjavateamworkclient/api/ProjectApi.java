e25f37b0845891cf6b6a2ab34599ab41688b8aa1
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-08-27 20:04:54 +0530

    retrofit interface for making requests to the api for getting project info

---
+++ TeamworkApiDemo/rxjavateamworkclient/src/main/java/com/vishnus1224/rxjavateamworkclient/api/ProjectApi.java
@@ -0,0 +1,24 @@
+package com.vishnus1224.rxjavateamworkclient.api;
+
+import com.vishnus1224.rxjavateamworkclient.model.ProjectResponseWrapper;
+
+import java.util.Map;
+
+import retrofit2.http.GET;
+import retrofit2.http.QueryMap;
+import rx.Observable;
+
+/**
+ * Created by Vishnu on 8/27/2016.
+ */
+public interface ProjectApi {
+
+    /**
+     * Retrieve all projects from the api.
+     * @param queryMap Map containing query params.
+     * @return Observable containing a list of projects.
+     */
+    @GET("/projects.json")
+    Observable<ProjectResponseWrapper> getAllProjects(@QueryMap Map<String, String> queryMap);
+
+}
