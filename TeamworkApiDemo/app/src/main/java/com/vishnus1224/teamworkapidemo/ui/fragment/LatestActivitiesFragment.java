5cd64991949aa36f5b6bf4c8efa36d1ace0a4b4c
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-08-27 12:03:17 +0530

    added image view to show the type of activity, removed unused imports

--- TeamworkApiDemo/app/src/main/java/com/vishnus1224/teamworkapidemo/ui/fragment/LatestActivitiesFragment.java
+++ TeamworkApiDemo/app/src/main/java/com/vishnus1224/teamworkapidemo/ui/fragment/LatestActivitiesFragment.java
@@ -16,7 +16,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.ProgressBar;
 import android.widget.SearchView;
 import android.widget.Toast;
 
