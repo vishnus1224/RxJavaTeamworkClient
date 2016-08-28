453a9412e0d3df2d0a4a8d52443ea0704d79d7b2
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-08-28 12:33:53 +0530

    helper class for converting dates to string and vice versa

---
+++ TeamworkApiDemo/app/src/main/java/com/vishnus1224/teamworkapidemo/util/DateTimeHelper.java
@@ -0,0 +1,57 @@
+package com.vishnus1224.teamworkapidemo.util;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
+/**
+ * Helper class for handling operations related to dates.
+ * Created by Vishnu on 8/28/2016.
+ */
+public final class DateTimeHelper {
+
+    private static final String ZULU_DATE_TIME_PATTERN = "yyyy-MM-dd'T'hh:mm:ss'Z'";
+
+    private static final String TIME_AM_PM_PATTERN = "E hh:mm a";
+
+    /**
+     * Convert the date string in the latest activity response to a date.
+     * @param dateString string to parse.
+     * @return date object.
+     */
+    public Date latestActivityDateTimeToDate(String dateString) throws ParseException {
+
+        return convertStringToDate(dateString, ZULU_DATE_TIME_PATTERN,
+                TimeZone.getTimeZone(TimeZone.getDefault().getDisplayName()));
+
+    }
+
+    public String extractDayAndTimeFromDate(Date date){
+
+        return formatDate(date, TIME_AM_PM_PATTERN, TimeZone.getDefault());
+
+    }
+
+    private Date convertStringToDate(String dateString, String pattern, TimeZone timeZone) throws ParseException {
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern, Locale.getDefault());
+
+        simpleDateFormat.setTimeZone(timeZone);
+
+        return simpleDateFormat.parse(dateString);
+
+    }
+
+    private String formatDate(Date date, String pattern, TimeZone timeZone){
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern, Locale.getDefault());
+
+        simpleDateFormat.setTimeZone(timeZone);
+
+        return simpleDateFormat.format(date);
+
+    }
+
+}
