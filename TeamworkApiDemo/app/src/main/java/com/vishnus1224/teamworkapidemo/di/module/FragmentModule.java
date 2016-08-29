7b01c6e333168c3fa9ff44ce75633f65387c1da9
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-08-29 23:44:27 +0530

    provided the mapper

--- TeamworkApiDemo/app/src/main/java/com/vishnus1224/teamworkapidemo/di/module/FragmentModule.java
+++ TeamworkApiDemo/app/src/main/java/com/vishnus1224/teamworkapidemo/di/module/FragmentModule.java
@@ -2,7 +2,15 @@ package com.vishnus1224.teamworkapidemo.di.module;
 
 import android.support.v4.app.Fragment;
 
+import com.vishnus1224.teamworkapidemo.di.scope.PerFragment;
+import com.vishnus1224.teamworkapidemo.mapper.Mapper;
+import com.vishnus1224.teamworkapidemo.mapper.RealmResultToListMapper;
+import com.vishnus1224.teamworkapidemo.model.LatestActivityModel;
+
+import javax.inject.Named;
+
 import dagger.Module;
+import dagger.Provides;
 
 /**
  * Created by vishnu on 24/08/16.
@@ -16,5 +24,11 @@ public class FragmentModule {
         this.fragment = fragment;
     }
 
+    @Provides @PerFragment @Named("realmResultToList")
+    Mapper provideMapper(RealmResultToListMapper<LatestActivityModel> realmResultToListMapper){
+
+        return realmResultToListMapper;
+
+    }
 
 }
--- TeamworkApiDemo/app/src/main/java/com/vishnus1224/teamworkapidemo/mapper/RealmResultToListMapper.java
+++ TeamworkApiDemo/app/src/main/java/com/vishnus1224/teamworkapidemo/mapper/RealmResultToListMapper.java
@@ -3,6 +3,8 @@ package com.vishnus1224.teamworkapidemo.mapper;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.inject.Inject;
+
 import io.realm.RealmModel;
 import io.realm.RealmResults;
 
@@ -10,6 +12,10 @@ import io.realm.RealmResults;
  * Created by Vishnu on 8/29/2016.
  */
 public class RealmResultToListMapper<T extends RealmModel> implements Mapper<RealmResults<T>, List<T>> {
+
+    @Inject
+    public RealmResultToListMapper() {
+    }
 
     @Override
     public List<T> map(RealmResults<T> ts) {
