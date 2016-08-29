3f21a0ccca850a7f6aac7c1e145ee48e87cd457a
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-08-29 23:30:25 +0530

    mapper for converting from realm results to list

---
+++ TeamworkApiDemo/app/src/main/java/com/vishnus1224/teamworkapidemo/mapper/RealmResultToListMapper.java
@@ -0,0 +1,23 @@
+package com.vishnus1224.teamworkapidemo.mapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.realm.RealmModel;
+import io.realm.RealmResults;
+
+/**
+ * Created by Vishnu on 8/29/2016.
+ */
+public class RealmResultToListMapper<T extends RealmModel> implements Mapper<RealmResults<T>, List<T>> {
+
+    @Override
+    public List<T> map(RealmResults<T> ts) {
+
+        List<T> list = new ArrayList<>(ts.size());
+
+        list.addAll(ts);
+
+        return list;
+    }
+}
