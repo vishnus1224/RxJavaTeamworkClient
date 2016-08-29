747bc79f33e0e918f5b4850737f192f20737d42a
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-08-30 00:19:59 +0530

    reduced the scope of the mapper to only map from latest activity model real results to list

--- TeamworkApiDemo/app/src/main/java/com/vishnus1224/teamworkapidemo/mapper/RealmResultToListMapper.java
+++ TeamworkApiDemo/app/src/main/java/com/vishnus1224/teamworkapidemo/mapper/RealmResultToListMapper.java
@@ -1,28 +1,31 @@
 package com.vishnus1224.teamworkapidemo.mapper;
 
+import com.vishnus1224.teamworkapidemo.model.LatestActivityModel;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
 
-import io.realm.RealmModel;
 import io.realm.RealmResults;
 
 /**
  * Created by Vishnu on 8/29/2016.
  */
-public class RealmResultToListMapper<T extends RealmModel> implements Mapper<RealmResults<T>, List<T>> {
+public class RealmResultToListMapper implements Mapper<RealmResults<LatestActivityModel>, List<LatestActivityModel>> {
 
     @Inject
     public RealmResultToListMapper() {
+
     }
 
+
     @Override
-    public List<T> map(RealmResults<T> ts) {
+    public List<LatestActivityModel> map(RealmResults<LatestActivityModel> latestActivityModels) {
 
-        List<T> list = new ArrayList<>(ts.size());
+        List<LatestActivityModel> list = new ArrayList<>(latestActivityModels.size());
 
-        list.addAll(ts);
+        list.addAll(latestActivityModels);
 
         return list;
     }
