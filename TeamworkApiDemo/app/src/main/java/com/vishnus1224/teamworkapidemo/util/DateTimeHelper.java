cb7fcc0ab6a1213d6393b9e943f5c4fc511a3430
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-08-31 15:50:40 +0530

    updated the pattern

--- TeamworkApiDemo/app/src/main/java/com/vishnus1224/teamworkapidemo/util/DateTimeHelper.java
+++ TeamworkApiDemo/app/src/main/java/com/vishnus1224/teamworkapidemo/util/DateTimeHelper.java
@@ -6,6 +6,8 @@ import java.util.Date;
 import java.util.Locale;
 import java.util.TimeZone;
 
+import javax.inject.Inject;
+
 /**
  * Helper class for handling operations related to dates.
  * Created by Vishnu on 8/28/2016.
@@ -14,9 +16,13 @@ public final class DateTimeHelper {
 
     private static final String ZULU_DATE_TIME_PATTERN = "yyyy-MM-dd'T'hh:mm:ss'Z'";
 
-    private static final String DUE_DATE_PATTERN = "YYYYMMDD";
+    private static final String DUE_DATE_PATTERN = "yyyyMMDD";
 
     private static final String TIME_AM_PM_PATTERN = "E hh:mm a";
+
+    @Inject
+    public DateTimeHelper() {
+    }
 
     /**
      * Convert the date string in the latest activity response to a date.
