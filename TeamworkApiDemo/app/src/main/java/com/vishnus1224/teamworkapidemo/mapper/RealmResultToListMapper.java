46b8e27812f46dabee01767d70a50b0e4873dcb2
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-09-01 14:05:29 +0530

    set dto as the return type

--- TeamworkApiDemo/app/src/main/java/com/vishnus1224/teamworkapidemo/mapper/RealmResultToListMapper.java
+++ TeamworkApiDemo/app/src/main/java/com/vishnus1224/teamworkapidemo/mapper/RealmResultToListMapper.java
@@ -1,5 +1,6 @@
 package com.vishnus1224.teamworkapidemo.mapper;
 
+import com.vishnus1224.teamworkapidemo.model.LatestActivityDto;
 import com.vishnus1224.teamworkapidemo.model.LatestActivityModel;
 
 import java.util.ArrayList;
@@ -12,7 +13,7 @@ import io.realm.RealmResults;
 /**
  * Created by Vishnu on 8/29/2016.
  */
-public class RealmResultToListMapper implements Mapper<RealmResults<LatestActivityModel>, List<LatestActivityModel>> {
+public class RealmResultToListMapper implements Mapper<RealmResults<LatestActivityModel>, List<LatestActivityDto>> {
 
     @Inject
     public RealmResultToListMapper() {
@@ -21,11 +22,65 @@ public class RealmResultToListMapper implements Mapper<RealmResults<LatestActivi
 
 
     @Override
-    public List<LatestActivityModel> map(RealmResults<LatestActivityModel> latestActivityModels) {
+    public List<LatestActivityDto> map(RealmResults<LatestActivityModel> latestActivityModels) {
 
-        List<LatestActivityModel> list = new ArrayList<>(latestActivityModels.size());
+        List<LatestActivityDto> list = new ArrayList<>(latestActivityModels.size());
 
-        list.addAll(latestActivityModels);
+        for(LatestActivityModel latestActivityModel : latestActivityModels){
+
+            LatestActivityDto latestActivityDto = new LatestActivityDto();
+
+            latestActivityDto.projectId = latestActivityModel.projectId;
+
+            latestActivityDto.itemId = latestActivityModel.itemId;
+
+            latestActivityDto.fromUserAvatarUrl = latestActivityModel.fromUserAvatarUrl;
+
+            latestActivityDto.description = latestActivityModel.description;
+
+            latestActivityDto.forUsername = latestActivityModel.forUsername;
+
+            latestActivityDto.publicInfo = latestActivityModel.publicInfo;
+
+            latestActivityDto.forUserId = latestActivityModel.forUserId;
+
+            latestActivityDto.itemLink = latestActivityModel.itemLink;
+
+            latestActivityDto.dateTime = latestActivityModel.dateTime;
+
+            latestActivityDto.activityType = latestActivityModel.activityType;
+
+            latestActivityDto.companyId = latestActivityModel.companyId;
+
+            latestActivityDto.projectName = latestActivityModel.projectName;
+
+            latestActivityDto.latestActivityType = latestActivityModel.latestActivityType;
+
+            latestActivityDto.link = latestActivityModel.link;
+
+            latestActivityDto.extraDescription = latestActivityModel.extraDescription;
+
+            latestActivityDto.isPrivate = latestActivityModel.isPrivate;
+
+            latestActivityDto.companyName = latestActivityModel.companyName;
+
+            latestActivityDto.id = latestActivityModel.id;
+
+            latestActivityDto.dueDate = latestActivityModel.dueDate;
+
+            latestActivityDto.lockDownId = latestActivityModel.lockDownId;
+
+            latestActivityDto.fromUsername = latestActivityModel.fromUsername;
+
+            latestActivityDto.type = latestActivityModel.type;
+
+            latestActivityDto.forUserAvatarUrl = latestActivityModel.forUserAvatarUrl;
+
+            latestActivityDto.userId = latestActivityModel.userId;
+
+            list.add(latestActivityDto);
+
+        }
 
         return list;
     }
