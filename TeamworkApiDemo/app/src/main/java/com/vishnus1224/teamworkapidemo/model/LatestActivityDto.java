6583cda8f34a2b56008ae6365fea055f27a4b288
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-09-01 14:04:54 +0530

    latest activity data transfer object

---
+++ TeamworkApiDemo/app/src/main/java/com/vishnus1224/teamworkapidemo/model/LatestActivityDto.java
@@ -0,0 +1,57 @@
+package com.vishnus1224.teamworkapidemo.model;
+
+import java.util.Date;
+
+/**
+ * Created by vishnu on 01/09/16.
+ */
+public class LatestActivityDto {
+
+    public String projectId;
+
+    public String itemId;
+
+    public String fromUserAvatarUrl;
+
+    public String description;
+
+    public String forUsername;
+
+    public String publicInfo;
+
+    public String forUserId;
+
+    public String itemLink;
+
+    public Date dateTime;
+
+    public String activityType;
+
+    public String companyId;
+
+    public String projectName;
+
+    public String latestActivityType;
+
+    public String link;
+
+    public String extraDescription;
+
+    public String isPrivate;
+
+    public String companyName;
+
+    public String id;
+
+    public Date dueDate;
+
+    public String lockDownId;
+
+    public String fromUsername;
+
+    public String type;
+
+    public String forUserAvatarUrl;
+
+    public String userId;
+}
