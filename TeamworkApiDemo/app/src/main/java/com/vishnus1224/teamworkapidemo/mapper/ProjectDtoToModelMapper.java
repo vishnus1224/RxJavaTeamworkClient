e9a848e6e2a659c36c433e66ea9c9cbd973a08f0
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-09-02 17:24:04 +0530

    project dto to project realm model mapper

---
+++ TeamworkApiDemo/app/src/main/java/com/vishnus1224/teamworkapidemo/mapper/ProjectDtoToModelMapper.java
@@ -0,0 +1,84 @@
+package com.vishnus1224.teamworkapidemo.mapper;
+
+import com.vishnus1224.teamworkapidemo.model.CategoryRealmModel;
+import com.vishnus1224.teamworkapidemo.model.ProjectDto;
+import com.vishnus1224.teamworkapidemo.model.ProjectRealmModel;
+
+import javax.inject.Inject;
+
+
+/**
+ * Created by vishnu on 02/09/16.
+ */
+public class ProjectDtoToModelMapper implements Mapper<ProjectDto, ProjectRealmModel> {
+
+    private TagDtoToModelListMapper tagDtoToModelListMapper;
+
+    private PeopleDtoToModelListMapper peopleDtoToModelListMapper;
+
+    @Inject
+    public ProjectDtoToModelMapper(TagDtoToModelListMapper tagDtoToModelListMapper, PeopleDtoToModelListMapper peopleDtoToModelListMapper) {
+
+        this.tagDtoToModelListMapper = tagDtoToModelListMapper;
+
+        this.peopleDtoToModelListMapper = peopleDtoToModelListMapper;
+    }
+
+    @Override
+    public ProjectRealmModel map(ProjectDto projectDto) {
+
+        ProjectRealmModel projectRealmModel = new ProjectRealmModel();
+
+        projectRealmModel.replyByEmailEnabled = projectDto.replyByEmailEnabled;
+
+        projectRealmModel.starred = projectDto.starred;
+
+        projectRealmModel.showAnnouncement = projectDto.showAnnouncement;
+
+        projectRealmModel.harvestTimersEnabled = projectDto.harvestTimersEnabled;
+
+        projectRealmModel.status = projectDto.status;
+
+        projectRealmModel.subStatus = projectDto.subStatus;
+
+        projectRealmModel.defaultPrivacy = projectDto.defaultPrivacy;
+
+        projectRealmModel.createdOn = projectDto.createdOn;
+
+        projectRealmModel.category = new CategoryRealmModel(projectDto.category.name, projectDto.category.id);
+
+        projectRealmModel.filesAutoNewVersion = projectDto.filesAutoNewVersion;
+
+        projectRealmModel.tags = tagDtoToModelListMapper.map(projectDto.tags);
+
+        projectRealmModel.logo = projectDto.logo;
+
+        projectRealmModel.startDate = projectDto.startDate;
+
+        projectRealmModel.id = projectDto.id;
+
+        projectRealmModel.lastChangeOn = projectDto.lastChangeOn;
+
+        projectRealmModel.people = peopleDtoToModelListMapper.map(projectDto.people);
+
+        projectRealmModel.endDate = projectDto.endDate;
+
+        projectRealmModel.name = projectDto.name;
+
+        projectRealmModel.privacyEnabled = projectDto.privacyEnabled;
+
+        projectRealmModel.description = projectDto.description;
+
+        projectRealmModel.announcement = projectDto.announcement;
+
+        projectRealmModel.isProjectAdmin = projectDto.isProjectAdmin;
+
+        projectRealmModel.startPage = projectDto.startPage;
+
+        projectRealmModel.notifyEveryone = projectDto.notifyEveryone;
+
+        projectRealmModel.announcementHTML = projectDto.announcementHTML;
+
+        return projectRealmModel;
+    }
+}
