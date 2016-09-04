b5bc0f0d2761748c7336ec3172e278d5e1d42f2d
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-09-04 19:22:43 +0530

    interface representing ui events to be shown on the projects screen

---
+++ TeamworkApiDemo/app/src/main/java/com/vishnus1224/teamworkapidemo/ui/view/ProjectsView.java
@@ -0,0 +1,27 @@
+package com.vishnus1224.teamworkapidemo.ui.view;
+
+import com.vishnus1224.teamworkapidemo.model.ProjectDto;
+
+import java.util.List;
+
+/**
+ * Created by Vishnu on 9/4/2016.
+ */
+public interface ProjectsView extends BaseView {
+
+    void showProjects(List<ProjectDto> projectDtoList);
+
+    void showError();
+
+    void showProjectsView();
+
+    void hideProjectsView();
+
+    void showNoProjectsView();
+
+    void hideNoProjectsView();
+
+    void showProgressBar();
+
+    void hideProgressBar();
+}
